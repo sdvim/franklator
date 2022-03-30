1aad1da21d6bebdf1cd0f5af12f3ce66bd8ab85a
Steve Della Valentina <s.dellavalentina@gmail.com>
2022-03-30 17:29:46 -0600

    frank

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const morse = { 
+  'a': '.-',    'b': '-...',  'c': '-.-.', 'd': '-..',
+  'e': '.',     'f': '..-.',  'g': '--.',  'h': '....',
+  'i': '..',    'j': '.---',  'k': '-.-',  'l': '.-..',
+  'm': '--',    'n': '-.',    'o': '---',  'p': '.--.',
+  'q': '--.-',  'r': '.-.',   's': '...',  't': '-',
+  'u': '..-',   'v': '...-',  'w': '.--',  'x': '-..-',
+  'y': '-.--',  'z': '--..',
+  '1': '.----', '2': '..---', '3': '...--', '4': '....-', 
+  '5': '.....', '6': '-....', '7': '--...', '8': '---..', 
+  '9': '----.', '0': '-----', 
+  '.': '.-.-.-',
+  ',': '--..--',
+  '?': '..--..',
+  '\'': '.----.',
+  '!': '-.-.--',
+  '/': '-..-.',
+  '(': '-.--.',
+  ')': '-.--.-',
+  '&': '.-...',
+  ':': '---...',
+  ';': '-.-.-.',
+  '=': '-...-',
+  '+': '.-.-.',
+  '-': '-....-',
+  '_': '..--.-',
+  '"': '.-..-.',
+  '$': '...-..-',
+  '@': '.--.-.',
+  '¿': '..-.-',
+  '¡': '--...-',
+};
+
+const morseReversed = Object.fromEntries(Object.entries(morse).map(([k, v]) => [v, k]));
+
+function isFrank(input) {
+  return /^[frank FRANK]+$/.test(input);
+}
+
+function toFrank(english) {
+  return english
+    .split('')
+    .map((c) => morse[c.toLowerCase()] || '')
+    .join(' ')
+    .replace(/\./g, 'frank')
+    .replace(/-/g, 'FRANK')
+    .replace(/ +/g, ' ');
+}
+
+function toEnglish(frank) {
+  return frank
+    .replace(/frank/g, '.')
+    .replace(/FRANK/g, '-')
+    .split(' ')
+    .map((w) => morseReversed[w] || '')
+    .join('')
+    .replace(/ +/g, ' ');
+}
+
 function App() {
+  const [englishText, setEnglishText] = useState("");
+  const [frankText, setFrankText] = useState("");
+
+  const handleEnglishChange = e => {
+    const value = e.target.value.toLowerCase();
+    if (value && !morse[value.substr(-1)]) return false;
+    setEnglishText(value);
+    setFrankText(toFrank(value));
+  };
+
+  const handleFrankChange = e => {
+    const value = e.target.value.replace(/(\r\n|\n|\r)/gm, "");
+    if (value && !isFrank(value)) return false;
+    setFrankText(value);
+    setEnglishText(toEnglish(value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <textarea
+        onChange={handleEnglishChange}
+        value={englishText}
+        placeholder="???"
+      />
+      <textarea
+        onChange={handleFrankChange}
+        value={frankText}
+        placeholder="frank"
+      />
+    </>
   );
 }
 
